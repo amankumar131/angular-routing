f06dbd01dc77853c11196d4a53b99b2ef78c365d
Aman Kumar <amankumar9092004@gmail.com>
2024-04-17 05:41:28 +0530

    Initial commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+
+export const routes: Routes = [
+    {'path': '', component: HomeComponent},
+    {'path': 'about', component: AboutComponent},
+    {'path': 'contact', component: ContactComponent},
+    {'path': 'navbar', component: NavbarComponent},
+];
